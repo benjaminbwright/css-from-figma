4e83eb7c7a2cc9f51ed4a6fe7d72c8c7a3c8b17e
Benjamin Wright <bwright@trilogyed.com>
2022-04-22 13:24:18 -0500

    setup basic class to wrap figma object methods

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // Dependencies
 const fs = require("fs/promises")
+
 // Data
 let css = `/* css */`;
 
-fs.writeFile("output/style.css", css).then(() => {
-  console.log("CSS successfully generated.")
-})
+
+// Classes 
+class FigmaObject {
+  constructor(options) {
+
+  }
+
+  outputCSS() {
+    fs.writeFile("output/style.css", css).then(() => {
+      console.log("CSS successfully generated.")
+    })
+  }
+
+}
+
+// Functions
+const init = function() {
+  const figma = new FigmaObject("hats");
+  figma.outputCSS();
+}
+
+
+// Interactions
+
+// Init
+init();
